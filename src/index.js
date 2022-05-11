4c70dfc4517cb39ece17b27f9605d15a43e51fb8
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-11 19:24:48 +0100

    Modified the index.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import createList from './display.js';
 
 const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 const gameID = '81kYyJKWPP59Yc9k4NXm'; //
@@ -45,18 +46,11 @@ inputs.forEach((input) => {
   });
 });
 
-const createList = async () => {
-  await fetch(requestURL)
-    .then((response) => response.json())
-    .then((json) => {
-      scoreList.innerHTML = `${json.result.sort((a, b) => b.score - a.score)
-        .map((score) => `<li class="scoreItem">${score.user}: ${score.score}</li>`).join('')}`;
-    });
-};
-
 createList();
 
 const refreshBtn = document.querySelector('.refresh');
 refreshBtn.addEventListener('click', () => {
   createList();
 });
+
+export { requestURL, scoreList };
