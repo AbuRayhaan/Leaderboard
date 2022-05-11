97a296d0966504b0527c87da786eb559c9b8869e
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-11 18:21:18 +0100

    Removed the modules dir

--- src/modules/error.js
+++
@@ -1,8 +0,0 @@
-const ErrorMsg = (error) => {
-  document.querySelector('.error-msg').innerHTML = error;
-  setTimeout(() => {
-    document.querySelector('.error-msg').innerHTML = '';
-  }, 2000);
-};
-
-export default ErrorMsg;
