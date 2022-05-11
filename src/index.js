258fcf052c4f928c0d27fee3337788a23a002548
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-11 10:10:16 +0100

    Generated the game Id

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import './style.css';
+
+// const refreshBtn = document.querySelector('.refresh');
+// const form = document.querySelector('form');
+
+const data = { "name": "Hammed Game" };
+
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0wasy4f9wnQ6KNT6X9R5/scores/', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+
+/*
 import ErrorMsg from './modules/error.js';
 import displayScore from './modules/display.js';
 
@@ -43,3 +65,4 @@ if (scoreList !== null) {
 } else {
   scoreList.classList.remove('list-border');
 }
+*/
