ded29828fa893ea23e9aceed4b002062f4acfab7
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-11 10:44:02 +0100

    Modified the index.js function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import './style.css';
 
-// const refreshBtn = document.querySelector('.refresh');
-// const form = document.querySelector('form');
+const refreshBtn = document.querySelector('.refresh');
+const form = document.querySelector('form');
 
-const data = { "name": "Hammed Game" };
-/*
-fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0wasy4f9wnQ6KNT6X9R5/scores/', {
-  method: 'POST', // or 'PUT'
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
-*/
+const createScore = (val) => {
+  const scoreList = document.querySelector('#score-list');
+  scoreList.innerHTML = '';
 
+  val.foreach((game, index) => {
+    const li = document.createElement('li');
+    li.textContent = `${game.user}: ${game.score}`;
+    li.id = index;
+    scoreList.appendChild(li);
+  });
+};
 
 const getScore = async () => {
   const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0wasy4f9wnQ6KNT6X9R5/scores/');
@@ -40,3 +33,19 @@ const setScore = async (score) => {
   const { data } = await response.json();
   return data;
 };
+
+const displayScores = async () => {
+  const result = await getScore();
+  createScore(result);
+};
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const { userName, userScore } = form.elements;
+  const game = { user: userName.value, score: userScore.value };
+  form.reset();
+  await setScore(game);
+  displayScores();
+});
+
+refreshBtn.addEventListener('click', displayScores);
