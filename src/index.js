602ef0fb0c2491f063082864a7ce11a10a66a9ed
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-11 10:19:38 +0100

    Modified the index.js to create the get and set score functions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './style.css';
 // const form = document.querySelector('form');
 
 const data = { "name": "Hammed Game" };
-
+/*
 fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0wasy4f9wnQ6KNT6X9R5/scores/', {
   method: 'POST', // or 'PUT'
   headers: {
@@ -19,50 +19,24 @@ fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0was
 .catch((error) => {
   console.error('Error:', error);
 });
-
-/*
-import ErrorMsg from './modules/error.js';
-import displayScore from './modules/display.js';
-
-let scores = [];
-const addButton = document.querySelector('#add-score');
-const scoreList = document.querySelector('#score-list');
-
-const addScores = (name, score) => {
-  const id = Date.now();
-  const object = { id, name, score };
-  if (name === '' || score === '') {
-    ErrorMsg('Kindly fill the fields');
-  } else {
-    scores.push(object);
-    localStorage.setItem('score', JSON.stringify(scores));
-    document.getElementById('userName').value = '';
-    document.getElementById('userScore').value = '';
-    displayScore(object.id, object.name, object.score);
-  }
-};
-
-const getScoreFromStorage = JSON.parse(localStorage.getItem('score'));
-if (getScoreFromStorage) {
-  scores = getScoreFromStorage;
-}
-
-scores.forEach((index) => {
-  displayScore(index.id, index.name, index.score);
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  addButton.addEventListener('click', (n) => {
-    n.preventDefault();
-    const name = document.getElementById('userName').value;
-    const score = document.getElementById('userScore').value;
-    addScores(name, score);
-  });
-});
-
-if (scoreList !== null) {
-  scoreList.classList.add('list-border');
-} else {
-  scoreList.classList.remove('list-border');
-}
 */
+
+
+const getScore = async () => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0wasy4f9wnQ6KNT6X9R5/scores/');
+  const { data } = await response.json();
+  return data;
+}
+
+const setScore = async (score) => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0wasy4f9wnQ6KNT6X9R5/scores/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(score),
+  });
+
+  const { data } = await response.json();
+  return data;
+};
